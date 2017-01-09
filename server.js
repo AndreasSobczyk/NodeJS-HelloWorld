268aba8b01ed4339d7260329fb9bade3ba793128
AndreasSobczyk <andreas.sobczyk@gmail.com>
2017-01-09 13:33:21 +0100

    new update!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello World! ASOB release to prod');
+    res.send('Hello World! release test');
 });
 
 var server = app.listen(port, function () {
